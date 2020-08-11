b871a15f4e82aca44e8be4b1857a8a4b4b8cec69
Martin Großmann <martin.grossmann@t-systems.com>
2020-08-11 17:04:14 +0200

    Initial commit

---
+++ src/main/java/de/mgrossmann/fritzdata/Main.java
@@ -0,0 +1,14 @@
+package de.mgrossmann.fritzdata;
+
+/**
+ * Created on 11.08.2020
+ *
+ * @author mgn
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Foobar");
+    }
+
+}
